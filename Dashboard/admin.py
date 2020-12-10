94f59f85a11f5416d415d66875366879658b5138
sapcresta <sapshrestha4@gmail.com>
2020-12-10 10:49:35 +0545

    dev environment

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Dataset, River, WarningLevel, WaterLevel, DangerLevel, Station, WaterLevelStatus
+
+admin.site.register(Dataset)
+admin.site.register(River)
+admin.site.register(WarningLevel)
+admin.site.register(WaterLevel)
+admin.site.register(WaterLevelStatus)
+admin.site.register(DangerLevel)
+admin.site.register(Station)
