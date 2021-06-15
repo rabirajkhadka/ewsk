06ec10bb03a663f5852e4880baa547c51a818268
winter-fuyu <thpbirat@gmail.com>
2021-06-15 12:58:30 +0545

    Added Django Cleanup for removing images when new images are uploaded from admin

--- drainage-api/drainageAPI/settings.py
+++ drainage-api/drainageAPI/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'webapp',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
--- drainage-api/webapp/admin.py
+++ drainage-api/webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Station, WarningMessage, WarningMessageData, DrainageTrend, Average, DataSet
 
+
 admin.site.register(Station)
 admin.site.register(WarningMessage)
 admin.site.register(WarningMessageData)
