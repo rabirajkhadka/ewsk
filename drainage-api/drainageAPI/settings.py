b440a656afe1eaf83a818070b13d8e336f903994
Sahaj Raj Malla <mallasahajraj@gmail.com>
2021-10-29 18:44:00 +0545

    added mysql configuration

--- drainage-api/drainageAPI/settings.py
+++ drainage-api/drainageAPI/settings.py
@@ -95,13 +95,24 @@ WSGI_APPLICATION = 'drainageAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
